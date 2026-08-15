c40157f19467c91f94ca0581f430147f3c60d0bb
kwakkyungmok-del <kwak.kyungmok@gmail.com>
2026-08-15 14:27:42 +0900

    Update rhythm-walk.js

--- asdf/tracks/rhythm-walk.js
+++ asdf/tracks/rhythm-walk.js
@@ -1,1 +1,42 @@
+const rhythmWalk = {
+    id: "rhythm-walk",
 
+    title: "Rhythm Walk",
+    artist: "Joyful Rhythm Walk",
+
+    audio: "../../lightbeatsmusic-joyful-rhythm-walk-funk-513936.mp3",
+
+    difficulty: "Easy",
+
+    bpm: 100,
+
+    duration: 120,
+
+    notes: [
+
+        // 예시 노트
+        // time = 노트가 판정선에 도착해야 하는 시간(ms)
+        // lane = 0 ~ 3
+
+        { time: 2000, lane: 0 },
+        { time: 2500, lane: 1 },
+        { time: 3000, lane: 2 },
+        { time: 3500, lane: 3 },
+
+        { time: 4500, lane: 0 },
+        { time: 5000, lane: 2 },
+        { time: 5500, lane: 1 },
+        { time: 6000, lane: 3 },
+
+        { time: 7000, lane: 0 },
+        { time: 7500, lane: 1 },
+        { time: 8000, lane: 3 },
+        { time: 8500, lane: 2 },
+
+        { time: 9500, lane: 0 },
+        { time: 10000, lane: 2 },
+        { time: 10500, lane: 1 },
+        { time: 11000, lane: 3 }
+
+    ]
+};
